f0d703284c5e7bb10b67888971306d4c923e6bc3
haydonryan <hryan@pivotal.io>
2014-11-11 20:35:00 -0800

    Templates working now

--- generate_stub.rb
+++ generate_stub.rb
@@ -1,85 +1,78 @@
 #!/usr/bin/ruby
 
 require 'erb'
-class Foo
-    attr_accessor :PLACEHOLDER_NAME,
-:PLACEHOLDER_DIRECTOR_UUID,
-:PLACEHOLDER_ACCESS_KEY_ID,
-:PLACEHOLDER_SECRET_KEY,
-:PLACEHOLDER_SUBNET_FOR_AZ1,
-:PLACEHOLDER_SUBNET_FOR_AZ2,
-:PLACEHOLDER_DOMAIN,
-:PLACEHOLDER_UAA_CERT,
-:PLACEHOLDER_NATS_USER,
-:PLACEHOLDER_NATS_PASSWORD,
-:PLACEHOLDER_CC_DB_ENCRYPTION_KEY,
-:PLACEHOLDER_BULK_API_PASSWORD,
-:PLACEHOLDER_STAGING_UPLOAD_PASSWORD,
-:PLACEHOLDER_STAGING_UPLOAD_USER,
-:PLACEHOLDER_UAA_ADMIN_CLIENT_SECRET,
-:PLACEHOLDER_UAA_JWT_SIGNING_KEY,  # Use the YAML "|" character to format multiline RSA key data
-:PLACEHOLDER_UAA_JWT_VERIFICATION_KEY, # Use the YAML "|" character to format multiline RSA key data
-:PLACEHOLDER_UAA_CLIENTS_LOGIN_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_DEVELOPER_CONSOLE_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_APP_DIRECT_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_SUPPORT_SERVICES_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_SERVICESMGMT_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_SPACE_MAIL_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_NOTIFICATION_SECRET,
-:PLACEHOLDER_UAA_BATCH_USERNAME,
-:PLACEHOLDER_UAA_BATCH_PASSWORD,
-:PLACEHOLDER_UAA_CC_CLIENT_SECRET,
-:PLACEHOLDER_UAADB_PROPERTIES,
-:PLACEHOLDER_CCDB_PROPERTIES,
-:PLACEHOLDER_ROUTER_STATUS_USER,
-:PLACEHOLDER_ROUTER_STATUS_PASSWORD,
-:PLACEHOLDER_LOGGREGATOR_SECRET
-      def template_binding
-            binding
-              end
+require 'ostruct'
+
+placeholders = {
+"placeholder_name"                                 => "hnrglobal",     #
+"placeholder_director_uuid"                        => "",
+"placeholder_access_key_id"                        => '456',
+"placeholder_secret_key"                           => '',
+"placeholder_subnet_for_az1"                       => '',
+"placeholder_subnet_for_az2"                       => '',
+"placeholder_domain"                               => '',
+"placeholder_uaa_cert"                             => '', 
+"placeholder_nats_user"                            => '',
+"placeholder_nats_password"                        => '',
+"placeholder_cc_db_encryption_key"                 => '',
+"placeholder_bulk_api_password"                    => '',
+"placeholder_staging_upload_password"              => '',
+"placeholder_staging_upload_user"                  => '',
+"placeholder_uaa_admin_client_secret"              => '',
+"placeholder_uaa_jwt_signing_key"                  => '',  # Use the YAML "|" character to format multiline RSA key data
+"placeholder_uaa_jwt_verification_key"             => '', # Use the YAML "|" character to format multiline RSA key data
+"placeholder_uaa_clients_login_secret"             => '',
+"placeholder_uaa_clients_developer_console_secret" => '',
+"placeholder_uaa_clients_app_direct_secret"        => '',
+"placeholder_uaa_clients_support_services_secret"  => '',
+"placeholder_uaa_clients_servicesmgmt_secret"      => '',
+"placeholder_uaa_clients_space_mail_secret"        => '',
+"placeholder_uaa_clients_notification_secret"      => '',
+"placeholder_uaa_batch_username"                   => '',
+"placeholder_uaa_batch_password"                   => '',
+"placeholder_uaa_cc_client_secret"                 => '',
+"placeholder_uaadb_properties"                     => '', 
+"placeholder_ccdb_properties"                      => '',
+"placeholder_router_status_user"                   => '',
+"placeholder_router_status_password"               => '',
+:PLACEHOLDER_LOGGREGATOR_SECRET                    => "HELL YES" 
+}
+
+class ErbBinding < OpenStruct
+    def get_binding
+        return binding()
+    end
 end
 
-=begin
-:PLACEHOLDER_NAME,
-:PLACEHOLDER_DIRECTOR_UUID,
-:PLACEHOLDER_ACCESS_KEY_ID,
-:PLACEHOLDER_SECRET_KEY,
-:PLACEHOLDER_SUBNET_FOR_AZ1,
-:PLACEHOLDER_SUBNET_FOR_AZ2,
-:PLACEHOLDER_DOMAIN,
-:PLACEHOLDER_UAA_CERT,
-:PLACEHOLDER_NATS_USER,
-:PLACEHOLDER_NATS_PASSWORD,
-:PLACEHOLDER_CC_DB_ENCRYPTION_KEY,
-:PLACEHOLDER_BULK_API_PASSWORD,
-:PLACEHOLDER_STAGING_UPLOAD_PASSWORD,
-:PLACEHOLDER_STAGING_UPLOAD_USER,
-:PLACEHOLDER_UAA_ADMIN_CLIENT_SECRET,
-:PLACEHOLDER_UAA_JWT_SIGNING_KEY,  # Use the YAML "|" character to format multiline RSA key data
-:PLACEHOLDER_UAA_JWT_VERIFICATION_KEY, # Use the YAML "|" character to format multiline RSA key data
-:PLACEHOLDER_UAA_CLIENTS_LOGIN_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_DEVELOPER_CONSOLE_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_APP_DIRECT_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_SUPPORT_SERVICES_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_SERVICESMGMT_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_SPACE_MAIL_SECRET,
-:PLACEHOLDER_UAA_CLIENTS_NOTIFICATION_SECRET,
-:PLACEHOLDER_UAA_BATCH_USERNAME,
-:PLACEHOLDER_UAA_BATCH_PASSWORD,
-:PLACEHOLDER_UAA_CC_CLIENT_SECRET,
-:PLACEHOLDER_UAADB_PROPERTIES,
-:PLACEHOLDER_CCDB_PROPERTIES,
-:PLACEHOLDER_ROUTER_STATUS_USER,
-:PLACEHOLDER_ROUTER_STATUS_PASSWORD,
-:PLACEHOLDER_LOGGREGATOR_SECRET
-=end
 
 
+data = {:bar => "baz"}
+vars = ErbBinding.new(placeholders)
 
-
-new_file = File.open("./result.txt", "w+")
+template = "foo <%= bar %>"
 template = File.read("./template.erb")
-foo = Foo.new
-foo.PLACEHOLDER_NATS_PASSWORD = "Hello"
-new_file << ERB.new(template).result(foo.template_binding)
-new_file.close
+erb = ERB.new(template)
+
+vars_binding = vars.send(:get_binding)
+puts erb.result(vars_binding)
+
+
+
+
+
+#ns = Namespace.new(placeholders)
+#print ERB.new(template).result(ns.instance_eval { binding })
+
+
+#puts placeholders
+
+#new_file = File.open("./result.txt", "w+")
+#puts ErbalT::render_from_hash(template, placeholders)
+#new_file.close
+
+#puts placeholders["PLACEHOLDER_LOGGREGATOR_SECRET"]
+
+#foo = Foo.new
+#foo.PLACEHOLDER_NATS_PASSWORD = "Hello"
+#new_file << ERB.new(template).result(foo.template_binding)
+#new_file.close
